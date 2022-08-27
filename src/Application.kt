fda945bea33a9533749df4f47355290bc9dc0380
ALI <ali11salem00@gmail.com>
2022-08-27 17:05:32 +0300

    JWT-Authentication

--- src/Application.kt
+++ src/Application.kt
@@ -1,6 +1,10 @@
 package com.example
 
+import com.example.authentication.JwtService
+import com.example.authentication.hash
+import com.example.data.model.User
 import com.example.repository.DatabaseFactory
+import com.example.repository.repo
 import io.ktor.application.*
 import io.ktor.response.*
 import io.ktor.request.*
@@ -18,6 +22,10 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 fun Application.module(testing: Boolean = false) {
 
     DatabaseFactory.init()
+    val db = repo()
+    val jwtService = JwtService()
+    val hashFunction = { s:String -> hash(s) }
+
     install(Sessions) {
         cookie<MySession>("MY_SESSION") {
             cookie.extensions["SameSite"] = "lax"
@@ -46,6 +54,16 @@ fun Application.module(testing: Boolean = false) {
         get("/note"){
             val id = call.request.queryParameters["id"]
             call.respond("${id}")
+        }
+
+        get("/token"){
+            val email = call.request.queryParameters["email"]!!
+            val password = call.request.queryParameters["password"]!!
+            val username = call.request.queryParameters["username"]!!
+
+            val user = User(email,hashFunction(password),username)
+            call.respond(jwtService.generateToken(user))
+
         }
 
         route("/notes"){
