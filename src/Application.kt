de610bf32e21ac943275a44d41e956b9e09f50a9
ALI <ali11salem00@gmail.com>
2022-08-27 17:35:21 +0300

    UserRoutes & Data Model

--- src/Application.kt
+++ src/Application.kt
@@ -5,6 +5,7 @@ import com.example.authentication.hash
 import com.example.data.model.User
 import com.example.repository.DatabaseFactory
 import com.example.repository.repo
+import com.example.routes.UserRoutes
 import io.ktor.application.*
 import io.ktor.response.*
 import io.ktor.request.*
@@ -12,8 +13,10 @@ import io.ktor.routing.*
 import io.ktor.http.*
 import io.ktor.sessions.*
 import io.ktor.auth.*
+import io.ktor.auth.jwt.*
 import io.ktor.gson.*
 import io.ktor.features.*
+import io.ktor.locations.*
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
@@ -34,7 +37,10 @@ fun Application.module(testing: Boolean = false) {
 
     install(Authentication) {
 
+
     }
+
+    install(Locations)
 
     install(ContentNegotiation) {
         gson {
@@ -42,29 +48,12 @@ fun Application.module(testing: Boolean = false) {
     }
 
     routing {
+
         get("/") {
             call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
         }
 
-        get("/note/{id}") {
-            val id = call.parameters["id"]
-            call.respond("${id}")
-        }
-
-        get("/note"){
-            val id = call.request.queryParameters["id"]
-            call.respond("${id}")
-        }
-
-        get("/token"){
-            val email = call.request.queryParameters["email"]!!
-            val password = call.request.queryParameters["password"]!!
-            val username = call.request.queryParameters["username"]!!
-
-            val user = User(email,hashFunction(password),username)
-            call.respond(jwtService.generateToken(user))
-
-        }
+        UserRoutes(db,jwtService,hashFunction)
 
         route("/notes"){
 
